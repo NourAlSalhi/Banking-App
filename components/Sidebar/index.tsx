57e53fbda97c1a46c149327f2b8b75f3707750e3
Nour Al Salhi <nuraym9998@gmail.com>
2024-10-28 11:31:41 +0200

    add strock to svg and color

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -9,7 +9,7 @@ import { usePathname } from "next/navigation";
 const Sidebar = ({ user }: SidebarProps) => {
   const isActive = usePathname();
   return (
-    <section className="sticky text-14 left-0 top-0 flex h-screen w-fit flex-col  border-r border-gray-200 bg-white pt-8 max-md:hidden sm:p-4 xl:p-6 md:w-[280px]">
+    <section className="sticky text-14 left-0 top-0 flex h-screen w-fit flex-col border-r border-gray-200 bg-white pt-8 max-md:hidden sm:p-4 xl:p-6 md:w-[280px]">
       <nav className="flex flex-col gap-4">
         <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
           <Image
@@ -24,10 +24,11 @@ const Sidebar = ({ user }: SidebarProps) => {
       </nav>
       <div className="h-[180px] flex flex-col justify-between">
         {sidebarLinks.map((item) => {
+          const IconComponent = item.imgURL;
           return (
             <Link
+              key={item.route}
               href={item.route}
-              key={item.label}
               className={`flex ${
                 isActive !== item.route && `hover:bg-slate-100`
               } items-center rounded px-3 py-2  gap-3 ${
@@ -36,10 +37,13 @@ const Sidebar = ({ user }: SidebarProps) => {
                   : "text-gray-800"
               }`}
             >
-              <div className="relative size-6">
-                <Image src={item.imgURL} alt={item.label} fill />
-              </div>
-              <p>{item.label}</p>
+              <IconComponent
+                className="w-6 h-6"
+                style={{
+                  stroke: isActive === item.route ? "white" : "none",
+                }}
+              />
+              <span>{item.label}</span>
             </Link>
           );
         })}
--- constants/index.ts
+++ constants/index.ts
@@ -1,21 +1,26 @@
+import HomeIcon from '/public/icons/home.svg';
+import DollarCircleIcon from '/public/icons/dollar-circle.svg';
+import TransactionIcon from '/public/icons/transaction.svg';
+import MoneySendIcon from '/public/icons/money-send.svg';
+
 export const sidebarLinks = [
     {
-      imgURL: "/icons/home.svg",
+      imgURL: HomeIcon,
       route: "/",
       label: "Home",
     },
     {
-      imgURL: "/icons/dollar-circle.svg",
+      imgURL: DollarCircleIcon,
       route: "/my-banks",
       label: "My Banks",
     },
     {
-      imgURL: "/icons/transaction.svg",
+      imgURL: TransactionIcon,
       route: "/transaction-history",
       label: "Transaction History",
     },
     {
-      imgURL: "/icons/money-send.svg",
+      imgURL: MoneySendIcon,
       route: "/payment-transfer",
       label: "Transfer Funds",
     },
