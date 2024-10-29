fc00c0e101855bbcaa09c988da759089a874769c
Nour Al Salhi <nuraym9998@gmail.com>
2024-10-29 10:06:35 +0200

    edit sidebar responsive for mid screen

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -9,7 +9,7 @@ import { usePathname } from "next/navigation";
 const Sidebar = ({ user }: SidebarProps) => {
   const isActive = usePathname();
   return (
-    <section className="sticky text-14 left-0 top-0 flex h-screen w-fit flex-col border-r border-gray-200 bg-white pt-8 max-md:hidden sm:p-4 xl:p-6 md:w-[280px]">
+    <section className="sticky text-14 left-0 top-0 flex h-screen max-xl:w-fit flex-col max-xl:items-center border-r border-gray-200 bg-white pt-8 max-md:hidden pr-3 ">
       <nav className="flex flex-col gap-4">
         <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
           <Image
@@ -17,9 +17,9 @@ const Sidebar = ({ user }: SidebarProps) => {
             width={34}
             height={34}
             alt="Horizon logo"
-            className="size-[24px] max-xl:size-14"
+            className="size-[30px] max-xl:size-10"
           />
-          <h1 className="sidebar-logo">Horizon</h1>
+          <h1 className="sidebar-logo hidden xl:block">Horizon</h1>
         </Link>
       </nav>
       <div className="h-[180px] flex flex-col justify-between">
@@ -29,9 +29,9 @@ const Sidebar = ({ user }: SidebarProps) => {
             <Link
               key={item.route}
               href={item.route}
-              className={`flex ${
+              className={`flex max-xl:w-fit ${
                 isActive !== item.route && `hover:bg-slate-100`
-              } items-center rounded px-3 py-2  gap-3 ${
+              } items-center max-xl:justify-center rounded px-3 py-2  gap-3 ${
                 isActive === item.route
                   ? "bg-[#0179FE] text-white font-semibold"
                   : "text-gray-800"
@@ -43,7 +43,7 @@ const Sidebar = ({ user }: SidebarProps) => {
                   stroke: isActive === item.route ? "white" : "none",
                 }}
               />
-              <span>{item.label}</span>
+              <span className="max-xl:hidden">{item.label}</span>
             </Link>
           );
         })}
